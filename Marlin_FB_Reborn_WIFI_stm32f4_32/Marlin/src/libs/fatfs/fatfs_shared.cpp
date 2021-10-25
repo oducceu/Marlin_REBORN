d4e1918fd722007d0305cd6466ccae8819d8014c
oducceu <oducceu@gmail.com>
2021-10-25 22:44:56 +0300

    Total Wipe

--- Marlin_FB_Reborn_WIFI_stm32f4_32/Marlin/src/libs/fatfs/fatfs_shared.cpp
+++
@@ -1,6 +0,0 @@
-#include "fatfs_shared.h"
-
-volatile FRESULT res;
-volatile FATFS FATFS_Obj;
-volatile DIR dir;
-volatile FILINFO fno;
--- Marlin_FB_Reborn_WIFI_stm32f4_32/Marlin/src/module/mks_wifi/mks_wifi_settings.h
+++
@@ -1,9 +0,0 @@
-#ifndef MKS_WIFI_SETTINGS_H
-#define MKS_WIFI_SETTINGS_H
-
-//#define MKS_WIFI_ENABLED_WIFI_CONFIG 
-
-#define MKS_WIFI_SSID					"network"
-#define MKS_WIFI_KEY					"password"
-
-#endif
